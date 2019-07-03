27bf0e739659750ed7e57db206da2aa4f1bc5cb3
Tatsu_K <kubo2019cooljapan@gmail.com>
2019-07-04 03:33:31 +0900

    no message

---
+++ UniMan/Assets/Script/SceneLoad.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneLoad : MonoBehaviour
+{
+
+    private AsyncOperation async;
+
+    [SerializeField] GameObject loadUI;
+
+    [SerializeField] private Slider slider;
+
+    public string SceneName;
+
+    static public SceneLoad instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
+    void LoadScene(string Name)
+    {
+        loadUI.SetActive(true);
+        StartCoroutine(LoadDeta(Name));
+    }
+
+    
+    IEnumerator LoadDeta(string Name)
+    {
+        async = SceneManager.LoadSceneAsync(Name);
+
+        while(!async.isDone)
+        {
+            slider.value = async.progress;
+            yield return null;
+        }
+
+           
+    }
+}
