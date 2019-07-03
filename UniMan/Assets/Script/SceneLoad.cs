2e2be91bdcc09baeb0b31d82671b9d9eb7502b06
Tatsu_K <kubo2019cooljapan@gmail.com>
2019-07-04 07:07:24 +0900

    no message

--- UniMan/Assets/Script/SceneLoad.cs
+++ UniMan/Assets/Script/SceneLoad.cs
@@ -14,9 +14,13 @@ public class SceneLoad : MonoBehaviour
 
     public string SceneName;
 
-    //static public SceneLoad instance;
+    static public SceneLoad instance;
+
+    private Texture2D blackTexture;
+    private float fadeAlpha = 0;
+    private bool Fading = false;
     // Start is called before the first frame update
-    /*
+
     void Awake()
     {
         if (instance == null)
@@ -30,25 +34,48 @@ public class SceneLoad : MonoBehaviour
 
             Destroy(gameObject);
         }
+        this.blackTexture = new Texture2D(32, 32, TextureFormat.RGB24, false);
+        this.blackTexture.ReadPixels(new Rect(0, 0, 32, 32), 0, 0, false);
+        this.blackTexture.SetPixel(0, 0, Color.white);
+        this.blackTexture.Apply();
     }
-    */
-    void LoadScene(string Name)
+    public void LoadScene(string Name)
     {
-        loadUI.SetActive(true);
-        StartCoroutine(LoadDeta(Name));
+        StartCoroutine(LoadDeta(Name,1f));
     }
 
-    
-    IEnumerator LoadDeta(string Name)
+    public void OnGUI()
     {
-        async = SceneManager.LoadSceneAsync(Name);
+        if (Fading)
+            return;
 
-        while(!async.isDone)
+        //透明度を更新して黒テクスチャを描画
+        GUI.color = new Color(0, 0, 0, this.fadeAlpha);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), this.blackTexture);
+    }
+
+
+    IEnumerator LoadDeta(string Name, float interval)
+    {
+        this.Fading = true;
+        float time = 0;
+        while (time <= interval)
         {
-            slider.value = async.progress;
-            yield return null;
+            this.fadeAlpha = Mathf.Lerp(0f, 1f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
+        }
+            async = SceneManager.LoadSceneAsync(Name);
+
+
+        time = 0;
+        while (time <= interval)
+        {
+            this.fadeAlpha = Mathf.Lerp(1f, 0f, time / interval);
+            time += Time.deltaTime;
+            yield return 0;
         }
 
-           
+        Fading = false;
     }
 }
